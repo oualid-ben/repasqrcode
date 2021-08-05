36d3f48db634198b5a342dcdbcfd8369641f9a2e
oualid-ben <72659499+oualid-ben@users.noreply.github.com>
2021-08-05 15:59:03 +0200

    Initial commit

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'quickqr';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['pre'] = 'qr_';
+$config['db']['port'] = '';
+
+$config['version'] = '6.1.1';
+?>
