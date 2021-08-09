2edd38f728fc6d3f0dc71385e55278595065afd4
jigard <jigar.d@aveosoft.com>
2021-08-09 11:41:35 +0530

    Commit for testing purpose

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,17 @@
 <?php
-$config['db']['host'] = 'localhost';
-$config['db']['name'] = 'quickqr';
-$config['db']['user'] = 'aveo';
-$config['db']['pass'] = 'Aveo_123';
-$config['db']['pre'] = 'qr_';
+// $config['db']['host'] = 'localhost';
+// $config['db']['name'] = 'quickqr';
+// $config['db']['user'] = 'aveo';
+// $config['db']['pass'] = 'Aveo_123';
+// $config['db']['pre'] = 'qr_';
+// $config['db']['port'] = '';
+
+
+$config['db']['host'] = 'eu-cdbr-west-01.cleardb.com';
+$config['db']['name'] = 'heroku_2e050316e1230e2';
+$config['db']['user'] = 'bbb18cb2af9d55';
+$config['db']['pass'] = 'dfaaa416';
+$config['db']['pre'] = '';
 $config['db']['port'] = '';
 
 $config['version'] = '6.1.1';
-$config['installed'] = '1';
-?>
