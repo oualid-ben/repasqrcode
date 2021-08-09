a38ce7f0350ce54b1b1cbf8b318a668e9be08e57
jigard <jigar.d@aveosoft.com>
2021-08-09 11:41:54 +0530

    Commit for testing purpose

--- includes/config.php
+++ includes/config.php
@@ -11,7 +11,7 @@ $config['db']['host'] = 'eu-cdbr-west-01.cleardb.com';
 $config['db']['name'] = 'heroku_2e050316e1230e2';
 $config['db']['user'] = 'bbb18cb2af9d55';
 $config['db']['pass'] = 'dfaaa416';
-$config['db']['pre'] = '';
+$config['db']['pre'] = 'qr_';
 $config['db']['port'] = '';
 
 $config['version'] = '6.1.1';
