ab25f1c9e9a510a5d1184681eecb37fe25a68f6f
jigard <jigar.d@aveosoft.com>
2021-08-09 11:31:20 +0530

    Commit for testing purpose

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,11 @@
 <?php
-$config['db']['host'] = 'eu-cdbr-west-01.cleardb.com';
-$config['db']['name'] = 'repasqrcode';
-$config['db']['user'] = 'bbb18cb2af9d55';
-$config['db']['pass'] = 'dfaaa416';
-$config['db']['pre'] = '';
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'quickqr';
+$config['db']['user'] = 'aveo';
+$config['db']['pass'] = 'Aveo_123';
+$config['db']['pre'] = 'qr_';
 $config['db']['port'] = '';
 
 $config['version'] = '6.1.1';
+$config['installed'] = '1';
 ?>
