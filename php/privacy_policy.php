02c0232ec9a716c6d3687a442bb5aa25cce7a2cf
jigard <jigar.d@aveosoft.com>
2021-08-21 16:02:51 +0530

    added privacy policypage

--- php/privacy_policy.php
+++ php/privacy_policy.php
@@ -1,44 +1,44 @@
 <?php
 // if testimonial is disable
-if (!$config['testimonials_enable']) {
-    error($lang['PAGE_NOT_FOUND'], __LINE__, __FILE__, 1);
-}
+// if (!$config['testimonials_enable']) {
+//     error($lang['PAGE_NOT_FOUND'], __LINE__, __FILE__, 1);
+// }
 
-if (checkloggedin()) {
-    update_lastactive();
-}
+// if (checkloggedin()) {
+//     update_lastactive();
+// }
 
-if (!isset($_GET['page']))
-    $page = 1;
-else
-    $page = $_GET['page'];
+// if (!isset($_GET['page']))
+//     $page = 1;
+// else
+//     $page = $_GET['page'];
 
-$limit = 9;
+// $limit = 9;
 
-$sql = "SELECT * FROM `" . $config['db']['pre'] . "privacy_policy`";
+// $sql = "SELECT * FROM `" . $config['db']['pre'] . "privacy_policy`";
 
-$total = mysqli_num_rows(mysqli_query($mysqli, "SELECT 1 FROM " . $config['db']['pre'] . "privacy_policy"));
-$query = "$sql LIMIT " . ($page - 1) * $limit . ",$limit";
+// $total = mysqli_num_rows(mysqli_query($mysqli, "SELECT 1 FROM " . $config['db']['pre'] . "privacy_policy"));
+// $query = "$sql LIMIT " . ($page - 1) * $limit . ",$limit";
 
-$result = ORM::for_table($config['db']['pre'] . 'privacy_policy')->raw_query($query)->find_many();
+// $result = ORM::for_table($config['db']['pre'] . 'privacy_policy')->raw_query($query)->find_many();
 
-$privacy_policy = array();
-if ($result) {
-    foreach ($result as $row) {
-        $privacy_policy[$row['id']]['id'] = $row['id'];
-        $privacy_policy[$row['id']]['name'] = $row['name'];
-        $privacy_policy[$row['id']]['designation'] = $row['designation'];
-        $privacy_policy[$row['id']]['content'] = $row['content'];
-        $privacy_policy[$row['id']]['image'] = !empty($row['image']) ? $row['image'] : 'default_user.png';
-    }
-}
+// $privacy_policy = array();
+// if ($result) {
+//     foreach ($result as $row) {
+//         $privacy_policy[$row['id']]['id'] = $row['id'];
+//         $privacy_policy[$row['id']]['name'] = $row['name'];
+//         $privacy_policy[$row['id']]['designation'] = $row['designation'];
+//         $privacy_policy[$row['id']]['content'] = $row['content'];
+//         $privacy_policy[$row['id']]['image'] = !empty($row['image']) ? $row['image'] : 'default_user.png';
+//     }
+// }
 
-$pagging = pagenav($total, $page, $limit, $link['LINK_PRIVACY_POLICY']);
+// $pagging = pagenav($total, $page, $limit, $link['LINK_PRIVACY_POLICY']);
 
 $page = new HtmlTemplate('templates/' . $config['tpl_name'] . '/privacy_policy.tpl');
-$page->SetParameter('OVERALL_HEADER', create_header($lang['LINK_PRIVACY_POLICY']));
-$page->SetLoop('LINK_PRIVACY_POLICY', $privacy_policy);
-$page->SetLoop('PAGES', $pagging);
-$page->SetParameter('SHOW_PAGING', (int)($total > $limit));
+$page->SetParameter('OVERALL_HEADER', create_header($lang['TESTIMONIALS']));
+// $page->SetLoop('LINK_PRIVACY_POLICY', $privacy_policy);
+// $page->SetLoop('PAGES', $pagging);
+// $page->SetParameter('SHOW_PAGING', (int)($total > $limit));
 $page->SetParameter('OVERALL_FOOTER', create_footer());
 $page->CreatePageEcho();
