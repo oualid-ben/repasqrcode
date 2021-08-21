06ba37045866ace174118c4d319826ec50079258
jigard <jigar.d@aveosoft.com>
2021-08-21 10:51:34 +0530

    changes in htacess

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author Bylancer
  * @url https://codecanyon.net/item/quickqr-contactless-restaurant-qr-menu-maker/29012439
@@ -8,10 +9,10 @@
 define("ROOTPATH", dirname(__FILE__));
 
 // Path to app folder.
-define("APPPATH", ROOTPATH."/php/");
+define("APPPATH", ROOTPATH . "/php/");
 
 // Check if SSL enabled
-if(!empty($_SERVER['HTTP_X_FORWARDED_PROTO']))
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']))
     $protocol = $_SERVER["HTTP_X_FORWARDED_PROTO"] == "https" ? "https://" : "http://";
 else
     $protocol = !empty($_SERVER['HTTPS']) && $_SERVER["HTTPS"] != "off" ? "https://" : "http://";
@@ -22,77 +23,81 @@ $site_url = $protocol
     . (dirname($_SERVER["SCRIPT_NAME"]) == DIRECTORY_SEPARATOR ? "" : "/")
     . trim(str_replace("\\", "/", dirname($_SERVER["SCRIPT_NAME"])), "/");
 
+
+echo $_SERVER["HTTP_HOST"];
+exit;
+
+
 define("SITEURL", $site_url);
 
-$config['app_url'] = SITEURL."/php/";
+$config['app_url'] = SITEURL . "/php/";
 //$config['site_url'] = SITEURL."/";
 
 require_once ROOTPATH . '/includes/classes/AltoRouter.php';
 
 // Start routing.
 $router = new AltoRouter();
- 
+
 $bp = trim(str_replace("\\", "/", dirname($_SERVER["SCRIPT_NAME"])), "/");
-$router->setBasePath($bp ? "/".$bp : "");
+$router->setBasePath($bp ? "/" . $bp : "");
 
 /* Setup the URL routing. This is production ready. */
 // Main routes that non-customers see
-$router->map('GET|POST','/', 'home.php');
-$router->map('GET|POST','/home/[a:lang]?/?', 'home.php');
-$router->map('GET|POST','/signup/?', 'signup.php');
-$router->map('GET|POST','/login/?', 'login.php');
-$router->map('GET|POST','/logout/?', 'logout.php');
-$router->map('GET|POST','/forgot/?', 'forgot.php');
-$router->map('GET|POST','/transaction/?', 'transaction.php');
-$router->map('GET|POST','/account-setting/?', 'account-setting.php');
-$router->map('GET|POST','/contact/?', 'contact.php');
-$router->map('GET|POST','/faq/?', 'faq.php');
-$router->map('GET|POST','/feedback/?', 'feedback.php');
-$router->map('GET|POST','/report/?', 'report.php');
-$router->map('GET|POST','/add-restaurant/?', 'add-restaurant.php');
-$router->map('GET|POST','/menu/?', 'menu.php');
-$router->map('GET|POST','/menu/[i:id]?/?', 'menu-edit.php');
-$router->map('GET|POST','/qr-builder/?', 'qr-builder.php');
-$router->map('GET|POST','/dashboard/?', 'dashboard.php');
-$router->map('GET|POST','/orders/?', 'orders.php');
-$router->map('GET|POST','/webhook/[*:i]?/?', 'webhook.php');
-$router->map('GET|POST','/invoice/[i:id]?/?', 'invoice.php');
-$router->map('GET|POST','/test/?', 'test.php');
+$router->map('GET|POST', '/', 'home.php');
+$router->map('GET|POST', '/home/[a:lang]?/?', 'home.php');
+$router->map('GET|POST', '/signup/?', 'signup.php');
+$router->map('GET|POST', '/login/?', 'login.php');
+$router->map('GET|POST', '/logout/?', 'logout.php');
+$router->map('GET|POST', '/forgot/?', 'forgot.php');
+$router->map('GET|POST', '/transaction/?', 'transaction.php');
+$router->map('GET|POST', '/account-setting/?', 'account-setting.php');
+$router->map('GET|POST', '/contact/?', 'contact.php');
+$router->map('GET|POST', '/faq/?', 'faq.php');
+$router->map('GET|POST', '/feedback/?', 'feedback.php');
+$router->map('GET|POST', '/report/?', 'report.php');
+$router->map('GET|POST', '/add-restaurant/?', 'add-restaurant.php');
+$router->map('GET|POST', '/menu/?', 'menu.php');
+$router->map('GET|POST', '/menu/[i:id]?/?', 'menu-edit.php');
+$router->map('GET|POST', '/qr-builder/?', 'qr-builder.php');
+$router->map('GET|POST', '/dashboard/?', 'dashboard.php');
+$router->map('GET|POST', '/orders/?', 'orders.php');
+$router->map('GET|POST', '/webhook/[*:i]?/?', 'webhook.php');
+$router->map('GET|POST', '/invoice/[i:id]?/?', 'invoice.php');
+$router->map('GET|POST', '/test/?', 'test.php');
 // Special (GET processing, etc)
 
-$router->map('GET|POST','/page/[*:id]?/?', 'html.php');
-$router->map('GET|POST','/membership/[a:change_plan]?/?', 'membership.php');
-$router->map('GET|POST','/ipn/[a:i]?/[*:id]?/?', 'ipn.php');
-$router->map('GET|POST','/payment/[*:token]?/[a:status]?/[*:message]?/?', 'payment.php');
-$router->map('GET','/sitemap.xml/?', 'xml.php');
-$router->map('GET|POST','/testimonials/?', 'testimonials.php');
-$router->map('GET|POST','/blog/?', 'blog.php');
-$router->map('GET|POST','/blog/category/[*:keyword]/?', 'blog-category.php');
-$router->map('GET|POST','/blog/author/[*:keyword]/?', 'blog-author.php');
-$router->map('GET|POST','/blog/[i:id]?/[*:slug]?/?', 'blog-single.php');
-$router->map('GET|POST','/whatsapp-ordering/?', 'whatsapp-ordering.php');
+$router->map('GET|POST', '/page/[*:id]?/?', 'html.php');
+$router->map('GET|POST', '/membership/[a:change_plan]?/?', 'membership.php');
+$router->map('GET|POST', '/ipn/[a:i]?/[*:id]?/?', 'ipn.php');
+$router->map('GET|POST', '/payment/[*:token]?/[a:status]?/[*:message]?/?', 'payment.php');
+$router->map('GET', '/sitemap.xml/?', 'xml.php');
+$router->map('GET|POST', '/testimonials/?', 'testimonials.php');
+$router->map('GET|POST', '/blog/?', 'blog.php');
+$router->map('GET|POST', '/blog/category/[*:keyword]/?', 'blog-category.php');
+$router->map('GET|POST', '/blog/author/[*:keyword]/?', 'blog-author.php');
+$router->map('GET|POST', '/blog/[i:id]?/[*:slug]?/?', 'blog-single.php');
+$router->map('GET|POST', '/whatsapp-ordering/?', 'whatsapp-ordering.php');
 
-$router->map('GET|POST','/restaurant/[i:id]?/[*:slug]?/?','restaurant.php'); // for old urls
-$router->map('GET|POST','/[*:slug]?/?','restaurant.php');
+$router->map('GET|POST', '/restaurant/[i:id]?/[*:slug]?/?', 'restaurant.php'); // for old urls
+$router->map('GET|POST', '/[*:slug]?/?', 'restaurant.php');
 
 // API Routes
 
 /* Match the current request */
-$match=$router->match();
+$match = $router->match();
 
 
-if($match) {
+if ($match) {
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        $_GET = array_merge($match['params'],$_GET);
+        $_GET = array_merge($match['params'], $_GET);
     }
 
     require_once ROOTPATH . '/includes/config.php';
 
-    if(!isset($config['installed']))
-    {
-        $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
-        $site_url = $protocol . $_SERVER['HTTP_HOST'] . str_replace ("index.php", "", $_SERVER['PHP_SELF']);
-        header("Location: ".$site_url."install/");
+    if (!isset($config['installed'])) {
+        $protocol = stripos($_SERVER['SERVER_PROTOCOL'], 'https') === true ? 'https://' : 'http://';
+        $site_url = $protocol . $_SERVER['HTTP_HOST'] . str_replace("index.php", "", $_SERVER['PHP_SELF']);
+        header("Location: " . $site_url . "install/");
         exit;
     }
 
@@ -107,22 +112,21 @@ if($match) {
     require_once ROOTPATH . '/includes/functions/func.sqlquery.php';
     require_once ROOTPATH . '/includes/classes/GoogleTranslate.php';
 
-    if(isset($_GET['lang'])) {
-        if ($_GET['lang'] != ""){
+    if (isset($_GET['lang'])) {
+        if ($_GET['lang'] != "") {
             change_user_lang($_GET['lang']);
         }
     }
-    require_once ROOTPATH . '/includes/lang/lang_'.$config['lang'].'.php';
+    require_once ROOTPATH . '/includes/lang/lang_' . $config['lang'] . '.php';
     require_once ROOTPATH . '/includes/seo-url.php';
 
     sec_session_start();
     $mysqli = db_connect();
 
 
-    require APPPATH.$match['target'];
-}
-else {
-	
-   header("HTTP/1.0 404 Not Found");
-   require APPPATH.'404.php';
+    require APPPATH . $match['target'];
+} else {
+
+    header("HTTP/1.0 404 Not Found");
+    require APPPATH . '404.php';
 }
